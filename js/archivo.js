46de536271bb2d8774f882a862e634ce48102636
flor condori <condorihuamanif@gmail.com>
2017-02-28 15:15:20 -0500

    implementacion de funcion Calcular IMC

--- js/archivo.js
+++ js/archivo.js
@@ -1,3 +1,5 @@
 function calcularMasa(){
-	return alert("Calculo Masa Corporal");
+var peso = document.getElementById('peso').value;
+var talla = document.getElementById('talla').value;
+	return alert("Indice Masa Corporal: "+(peso/Math.pow(talla,2)).toFixed(2));
 }
